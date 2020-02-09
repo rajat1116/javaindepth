855799e8e4fde46fe8e5a6a168576e7b1653478b
Rajat Pandit <rahulpandit106@gmail.com>
2020-02-09 15:23:40 +0530

    Basics demo

--- src/com/semanticsquare/basics/BasicsDemo.java
+++ src/com/semanticsquare/basics/BasicsDemo.java
@@ -1,5 +1,5 @@
 class BasicsDemo {
-  // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
+
   static void print() {
     System.out.println("\n\nInside print ...");
     System.out.println("Hello, world!!"); // Advance cursor to beginning of next line
@@ -12,6 +12,12 @@ class BasicsDemo {
   
   public static void main(String[] args) {	
 	   	// Language Basics 1
-		print();				
+		print();
+		int i = 6;
+		int j = 2;
+		System.out.println(i +j);
+		System.out.println(i-j);
+		System.out.println(i / j);
+		System.out.print(i * j);
   }       
 }
