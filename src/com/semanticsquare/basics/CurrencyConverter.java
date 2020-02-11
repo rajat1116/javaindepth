80cc7757adfe180c3f1ec1ab6105c69af21aa7a5
Rajat Pandit <rahulpandit106@gmail.com>
2020-02-11 05:57:56 +0530

    Arrays - CurrencyConverter

--- src/com/semanticsquare/basics/CurrencyConverter.java
+++ src/com/semanticsquare/basics/CurrencyConverter.java
@@ -1,5 +1,6 @@
 public class CurrencyConverter {
 
+    /*
     int rupee = 63;
     int dirham = 3; // UAE
     int real = 3;  // brazilian
@@ -9,18 +10,24 @@ public class CurrencyConverter {
     int $australian = 2;  // australian dollar
     int dollar;
     int Rupee = 63;
+     */
+
+    int[] exchangeRates = new int[] {63,3,3,595,18,107,2,0,63};
+
+
 
     void printCurrencies(){
-        System.out.println("rupee: " + rupee);
-        System.out.println("dirham: " + dirham);
-        System.out.println("real: "+real);
-        System.out.println("chilean_peso: "+chilean_peso);
-        System.out.println("mexican_peso:" + mexican_peso);
-        System.out.println("_yen: "+ _yen);
-        System.out.println("dollar: " + dollar);
-        System.out.println("Rupee: "+ Rupee);
-        System.out.println("$australian: " + $australian);
+        System.out.println("rupee: " + exchangeRates[0]);
+        System.out.println("dirham: " + exchangeRates[1]);
+        System.out.println("real: "+ exchangeRates[2]);
+        System.out.println("chilean_peso: "+ exchangeRates[3]);
+        System.out.println("mexican_peso:" + exchangeRates[4]);
+        System.out.println("_yen: "+ exchangeRates[5]);
+        System.out.println("dollar: " + exchangeRates[7]);
+        System.out.println("Rupee: "+ exchangeRates[8]);
+        System.out.println("$australian: " + exchangeRates[6]);
     }
+
     public static void main(String[] args){
         CurrencyConverter cc= new CurrencyConverter();
         cc.printCurrencies();
